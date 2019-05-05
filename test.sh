43830bb9c6116e401ef4900815e61de5ddbb4b33
josh <postor@gmail.com>
2019-05-05 10:13:20 +0800

    offline package

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 # write test file
+cat answers/$TESTKEY.js
 in=$(cat templates/${TESTKEYCAMEL}Test.txt)
 in2=$(cat answers/$TESTKEY.js)
 sep='//LEETCODE_MVP_ANSWER'
 echo "${in//$sep/$in2}" > src/tests/${TESTKEYCAMEL}Test.java
-
 # build and run
-mvn package dependency:copy-dependencies >> /dev/null
+mvn package dependency:copy-dependencies -o --legacy-local-repository
 java -cp .:target/*:target/dependency/* tests.${TESTKEYCAMEL}Test
